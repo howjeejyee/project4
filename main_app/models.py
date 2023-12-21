d4b3f36c49773a4a6a6033e4b86866746761e66c
howjeejyee <146084645+howjeejyee@users.noreply.github.com>
2023-12-21 09:00:55 +0800

    image updated

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,34 @@
 from django.db import models
-from django.urls import reverse
-from datetime import date
 from django.contrib.auth.models import User
 
+CLASS = (
+    ('W', 'Warrior'),
+    ('A', 'Archer'),
+    ('R', 'Rogue')
+)
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    weapon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 class Character(models.Model):
     name = models.CharField(max_length=100)
-    char_class = models.CharField(max_length=100)
+    char_class = models.CharField(
+        max_length=1,
+        choices=CLASS,
+        default=CLASS[0][0]
+    )
+
+    classjob = models.ForeignKey(
+        Class,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 	
@@ -9,3 +11,6 @@ urlpatterns = [
     path('start_game/', views.start_game, name='start_game'),
     path('accounts/signup/', views.signup, name='signup')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
